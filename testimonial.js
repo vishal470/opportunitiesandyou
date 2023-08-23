150293c4c6e9b02d8c25d5037bd0dce574b4c6f7
ranjanpriti <sharmavishal49313@gmail.com>
2023-08-23 08:15:24 +0530

    tesimoninal

--- testimonial.js
+++ testimonial.js
@@ -13,44 +13,72 @@ let isChickenVisible;
 let people = [
 	{
 		photo:
-			'url("https://cdn.pixabay.com/photo/2018/03/06/22/57/portrait-3204843_960_720.jpg")',
-		name: "Susan Smith",
-		profession: "WEB DEVELOPER",
+			'url("https://img.freepik.com/free-psd/3d-illustration-human-avatar-profile_23-2150671140.jpg?w=740&t=st=1692758337~exp=1692758937~hmac=e5a5354ccc84c3909a45f73f086ba1bb9d58de6bf8feba5217338f056ba06902")',
+		name: "Neha",
 		description:
-			"Cheese and biscuits chalk and cheese fromage frais. Cheeseburger caerphilly cheese slices chalk and cheese cheeseburger mascarpone danish fontina rubber cheese. Squirty cheese say cheese manchego jarlsberg lancashire taleggio cheese and wine squirty cheese. Babybel pecorino feta macaroni cheese brie queso everyone loves gouda. Cheese and biscuits camembert de normandie fromage fromage macaroni cheese"
+			"Working with Opportunities And You was a game-changer for our business. Their team's expertise in influencer marketing led to a seamless and successful campaign that brought us increased visibility and customer engagement."
 	},
 
 	{
 		photo:
-			"url('https://cdn.pixabay.com/photo/2019/02/11/20/20/woman-3990680_960_720.jpg')",
-		name: "Anna Grey",
-		profession: "UFC FIGHTER",
+			"url('https://img.freepik.com/free-psd/3d-illustration-human-avatar-profile_23-2150671122.jpg?w=740&t=st=1692758426~exp=1692759026~hmac=aac77e3308debf0e2f7cdbf55b36f50d935079efb0105fc7785a661710d2ee66')",
+		name: "Abhishek",
 		description:
-			"I'm baby migas cornhole hell of etsy tofu, pickled af cardigan pabst. Man braid deep v pour-over, blue bottle art party thundercats vape. Yr waistcoat whatever yuccie, farm-to-table next level PBR&B. Banh mi pinterest palo santo, aesthetic chambray leggings activated charcoal cred hammock kitsch humblebrag typewriter neutra knausgaard. Pabst succulents lo-fi microdosing portland gastropub Banh mi pinterest palo santo"
+			"ONU's free SEO name options gave our company the competitive edge we needed. Their expertise in influencer marketing turned our ordinary campaign into something extraordinary, and we couldn't be happier with the results!"
 	},
 
 	{
 		photo:
-			"url('https://cdn.pixabay.com/photo/2016/11/21/12/42/beard-1845166_960_720.jpg')",
-		name: "Branson Cook",
-		profession: "ACTOR",
+			"url('https://img.freepik.com/free-psd/3d-illustration-person-with-sunglasses_23-2149436180.jpg?w=740&t=st=1692758497~exp=1692759097~hmac=07e7efc2964653de7331f32f75c069108b7e0d4e45e7a6f2c4a84330d07ebe55')",
+		name: "Jyoti",
 		description:
-			"Radio telescope something incredible is waiting to be known billions upon billions Jean-François Champollion hearts of the stars tingling of the spine. Encyclopaedia galactica not a sunrise but a galaxyrise concept of the number one encyclopaedia galactica from which we spring bits of moving fluff. Vastness is bearable only through love paroxysm of global death concept"
+			"We were impressed with the personalized attention and care. ONU provided throughout the influencer marketing process. They understood our brand identity and tailored the campaign to perfectly align with our values."
 	},
 
 	{
 		photo:
-			"url('https://cdn.pixabay.com/photo/2014/10/30/17/32/boy-509488_960_720.jpg')",
-		name: "Julius Grohn",
-		profession: "PROFESSIONAL CHILD",
+			"url('https://img.freepik.com/free-psd/3d-illustration-person-with-sunglasses_23-2149436188.jpg?w=740&t=st=1692758578~exp=1692759178~hmac=c4292b6848e59f43cfae55d909b13a5d811a9f16bab67aca07a26aa3eb95e375')",
+		name: "Md. Farhan",
 		description:
-			"Biscuit chocolate pastry topping lollipop pie. Sugar plum brownie halvah dessert tiramisu tiramisu gummi bears icing cookie. Gummies gummi bears pie apple pie sugar plum jujubes. Oat cake croissant bear claw tootsie roll caramels. Powder ice cream caramels candy tiramisu shortbread macaroon chocolate bar. Sugar plum jelly-o chocolate dragée tart chocolate marzipan cupcake gingerbread."
+			"Opportunities And You's team demonstrated exceptional professionalism and creativity. Their innovative ideas and ability to execute flawless influencer collaborations made them stand out from other marketing companies."
+	},
+
+	{
+		photo:
+			"url('https://img.freepik.com/premium-photo/3d-rendering-male-character-profile-avatar-happy-young-man-with-bucket-hat-blue-clothes-good_477250-60.jpg?w=740')",
+		name: "Nitin Roy",
+		description:
+			"Our partnership with Opportunities And You exceeded our expectations in every way. Thanks to their expertise, our garment e-commerce company witnessed exponential growth and firmly established its position in the market."
+	},
+
+	{
+		photo:
+			"url('https://img.freepik.com/free-psd/3d-illustration-human-avatar-profile_23-2150671122.jpg?w=740&t=st=1692758426~exp=1692759026~hmac=aac77e3308debf0e2f7cdbf55b36f50d935079efb0105fc7785a661710d2ee66')",
+		name: "Arvind",
+		description:
+			"The results speak for themselves! ONU's influencer marketing campaign delivered an impressive return on investment, proving their expertise in the industry and confirming that they are the go-to company for any e-commerce business."
+	},
+
+	{
+		photo:
+			"url('https://img.freepik.com/free-psd/3d-illustration-human-avatar-profile_23-2150671122.jpg?w=740&t=st=1692758426~exp=1692759026~hmac=aac77e3308debf0e2f7cdbf55b36f50d935079efb0105fc7785a661710d2ee66')",
+		name: "Rohit",
+		description:
+			"Working with Opportunities And You felt like a true collaboration. They listened to our ideas and concerns and integrated them into the campaign seamlessly. Their willingness to adapt and improve continuously was commendable."
+	},
+
+	{
+		photo:
+			"url('https://img.freepik.com/free-psd/3d-illustration-person-with-long-hair_23-2149436197.jpg?w=740&t=st=1692758536~exp=1692759136~hmac=56d1fdf0dd8dc29e5cd50a51649a22566246dd093eccbe33ffe798b5d5e7fcde')",
+		name: "Renuka Rajalakshmi",
+		description:
+			"Working with Opportunities And You felt like having an extension of our in-house marketing team. They seamlessly integrated with our internal processes and brought fresh ideas to the table, elevating our marketing efforts to new heights."
 	}
 ];
 
 imgDiv.style.backgroundImage = people[0].photo;
 personName.innerText = people[0].name;
-profession.innerText = people[0].profession;
+// profession.innerText = people[0].profession;
 description.innerText = people[0].description;
 let currentPerson = 0;
 
@@ -91,9 +119,9 @@ function slide(whichSide, personNumber) {
 	setTimeout(() => {
 		personName.innerText = people[personNumber].name;
 	}, 400);
-	setTimeout(() => {
-		profession.innerText = people[personNumber].profession;
-	}, 400);
+	// setTimeout(() => {
+	// 	profession.innerText = people[personNumber].profession;
+	// }, 400);
 	setTimeout(() => {
 		description.innerText = people[personNumber].description;
 	}, 400);
